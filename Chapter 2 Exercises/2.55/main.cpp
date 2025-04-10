392ab67000ea7743736ea2581bf255f999d0c7e1
Edgar Yeghiazaryan <yeghiazaryanedgar93@gmail.com>
2025-04-10 17:39:48 +0400

    Expercise 2.55 update

--- Chapter 2 Exercises/2.55/main.cpp
+++ Chapter 2 Exercises/2.55/main.cpp
@@ -6,15 +6,11 @@ int main() {
 
     cout << "Pythagorean Triples (side1, side2, hypotenuse) up to " << limit << ":\n\n";
 
-    for (int side1 = 1; side1 <= limit; ++side1) {
-        for (int side2 = side1; side2 <= limit; ++side2) {  // avoid duplicates
-            for (int hypotenuse = side2; hypotenuse <= limit; ++hypotenuse) {
-                if (side1 * side1 + side2 * side2 == hypotenuse * hypotenuse) {
+    for (int side1 = 1; side1 <= limit; ++side1)
+        for (int side2 = side1; side2 <= limit; ++side2)
+            for (int hypotenuse = side2; hypotenuse <= limit; ++hypotenuse)
+                if (side1 * side1 + side2 * side2 == hypotenuse * hypotenuse)
                     cout << side1 << ", " << side2 << ", " << hypotenuse << endl;
-                }
-            }
-        }
-    }
 
     return 0;
 }
